3769fd74c71e5e47e664b5fe8568200e3dfce6e0
MatMacinf <148656211+MatMacinf@users.noreply.github.com>
2024-01-08 19:55:26 +0100

    Update Game.cpp

--- wisielec-serwer/Game.cpp
+++ wisielec-serwer/Game.cpp
@@ -2,6 +2,10 @@
 
 Game::Game()
 {
+    for(int i =0; i < 4; ++i){
+        Room newRoom;
+        gameRooms[i + 1] = newRoom;
+    }
 
 }
 
@@ -9,13 +13,46 @@ Game::~Game()
 {
 
 }
-
-void Game::addPlayerToRoom(int roomId, const Player& newPlayer) {
-    gameRooms[roomId].addPlayer(newPlayer); // Dodanie gracza do konkretnego pokoju
+Player* Game::getPlayerByUsername(const std::string& username){
+    auto it = playersMap.find(username);
+    if(it != playersMap.end()){
+        return &(it->second);
+    }
+    return nullptr;
 }
-
+bool Game::addPlayerToRoom(int roomId, Player* player) {
+    if(player)
+    {
+        if(gameRooms[roomId].getNumPlayers()<10){
+            gameRooms[roomId].addPlayer(*player);
+            return true;
+        }
+    }
+    return false;
+}
+bool Game::removePlayerFromRoom(int roomId, Player* player){
+    if(player){
+        gameRooms[roomId].removePlayer(*player);
+        return true;
+    }
+    return false;
+}
+std::unordered_map<int,Room>&Game::getGameRooms(){
+    return gameRooms;
+}
+void Game::addPlayertoMap(const std::string& username, const Player& player){
+    playersMap.insert({username, player});
+}
 std::string Game::getWordToGuess() {
     return wordToGuess;
+}
+void Game::createNewRoom(const Player& player){
+    int newRoomId = gameRooms.size() + 1;
+    Room newRoom;
+    gameRooms[newRoomId] = newRoom;
+    newRoom.addPlayer(player);
+
+
 }
 
 void Game::setWordToGuess(std::string word) {
