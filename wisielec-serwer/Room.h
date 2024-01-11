2d755b531e0f9f0f35e3b19594187e903cbb679b
MatMacinf <148656211+MatMacinf@users.noreply.github.com>
2024-01-11 15:28:02 +0100

    Update Room.h

--- wisielec-serwer/Room.h
+++ wisielec-serwer/Room.h
@@ -11,15 +11,23 @@ class Room
 {
 private:
     int roomId;
+    std::string wordtoguess;
     std::vector<Player> players;
+    std::unordered_set<char> guessedLetters;
     // Dodaj pola związane z grą, np. słowo, stan wisielca itp.
 public:
     Room();
     ~Room();
     void addPlayer(const Player& player);
+    std::string getFirstPlayer() const;
+    bool isLetterGuessed(char letter) const;
+    void addGuessedLetter(char letter);
     void removePlayer(const Player& playertoRemove);
     int getNumPlayers() const;
-    std::vector<std::string> getPlayers() const;
+    std::unordered_set<char> getGuessed();
+    void setWord(std::string word);
+    std::string getWord();
+    const std::vector<Player>& getPlayers() const;
 
     void startGame();
     void endGame();
