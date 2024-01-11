e8700e04b176c902100c47fa8a49b98821025931
MatMacinf <148656211+MatMacinf@users.noreply.github.com>
2024-01-11 15:27:22 +0100

    Update Player.cpp

--- wisielec-serwer/Player.cpp
+++ wisielec-serwer/Player.cpp
@@ -1,6 +1,6 @@
 #include "Player.h"
 
-Player::Player(const std::string& playerName, int playerSocket) : name(playerName), score(0), hangmanState(0), socket(playerSocket)
+Player::Player(const std::string& playerName, int playerSocket) : name(playerName), score(0), hangmanState(0), socket(playerSocket), roomId(0)
 {
 
 }
@@ -14,23 +14,30 @@ std::string Player::getName() const
 {
     return name;
 }
-
+void Player::setRoomId(int roomID) {
+    roomId = roomID;
+}
+int Player::getSocket() const {
+    return socket;
+}
 int Player::getScore() {
     return score;
 }
 
-void Player::updateScore(int points) {
-    score += points;
+void Player::updateScore() {
+    score += 1;
 }
 
 int Player::getHangmanState() {
     return hangmanState;
 }
 
-void Player::updateHangmanState(int newState) {
-    hangmanState = newState;
+void Player::updateHangmanState() {
+    hangmanState += 1;
 }
-
+void Player::resetHangmanState(){
+    hangmanState = 0;
+}
 bool Player::addNewPlayer(const std::string &playerName)
 {
     if (allPlayerNames.count(playerName) == 0) {
@@ -39,7 +46,9 @@ bool Player::addNewPlayer(const std::string &playerName)
     }
     return false;
 }
-
+int Player::getRoomId(){
+    return roomId;
+}
 bool Player::doesPlayerExist(const std::string &playerName)
 {
     return allPlayerNames.count(playerName) > 0;
